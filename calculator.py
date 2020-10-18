1e094318cd342b85cc3a8bca46d92616123eab14
superlff888 <1310157572@qq.com>
2020-10-18 23:16:16 +0800

    测试一下

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# ！./venv python
+# @Author : Mr.lee
+# @Date   ：2020-10-18 20:04
+#  -*-  utf-8  -*-
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
+
