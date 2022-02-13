70974ff8fe032f093ec4dfa34801028adab4997e
superlff888 <1310157572@qq.com>
2022-02-13 23:09:34 +0800

    新增.gitignore文件

--- myPythonBattle_pytest/com/calc.py
+++ myPythonBattle_pytest/com/calc.py
@@ -11,7 +11,7 @@ class Calc:
 
         if a > 99 or a < -99 or b > 99 or b < -99:
             print("请输入范围为【-99, 99】的整数或浮点数")
-            return "参数大小超出范围！！！！！！"
+            return "参数大小超出范围！qqqqqqqqq"
 
         return a + b
 
