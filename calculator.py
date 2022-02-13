5a46659f03a1fdc38e8ce469b76f2c8175e4c341
superlff888 <1310157572@qq.com>
2022-02-13 11:54:30 +0800

    提交 Version 3.0，代码error

--- calculator.py
+++ calculator.py
@@ -15,3 +15,19 @@ class Calculator:
     def div(self, a, b):
         return a / b
 
+
+class Calc:
+    def add(self, a, b):
+
+        if a > 99 or a < -99 or b > 99 or b < -99:
+            print("请输入范围为【-99, 99】的整数或浮点数")
+            return "参数大小超出范围"
+
+        return a + b
+
+    def div(self, a, b):
+        if a > 99 or a < -99 or b > 99 or b < -99:
+            print("请输入范围为【-99, 99】的整数或浮点数")
+            return "参数大小超出范围"
+
+        return a / b
