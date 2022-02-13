dda38055548a9cdea1a9c5ff98035f5fc0be4197
superlff888 <1310157572@qq.com>
2022-02-13 23:15:17 +0800

    新增.gitignore文件

--- myPythonBattle_pytest/com/calc.py
+++ myPythonBattle_pytest/com/calc.py
@@ -17,7 +17,7 @@ class Calc:
 
     def div(self, a, b):
         if a > 99 or a < -99 or b > 99 or b < -99:
-            print("请输入范围为[-99, 99]的整数或浮点数")
+            print("请输入范围为[-99, 99]的整数或zhe浮点数")
             return "参数大小超出范围,无法计算！!!"
 
         return a / b
