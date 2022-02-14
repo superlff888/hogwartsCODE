2ebfc1ecc0736d2d2e1baff01ba362bef5cf1e77
superlff888 <1310157572@qq.com>
2022-02-14 15:30:06 +0800

    更新conftest

--- pythonBattle_pytest/testCases/conftest.py
+++ pythonBattle_pytest/testCases/conftest.py
@@ -4,21 +4,18 @@
 # =============================================================
 
 """
-conftest文件必须放在包中（包含init.py文件）
+【该hook函数作用】
+    防止测试用例标题ids中文两乱码
+【注意】
+    conftest文件必须放在包中（包含init.py文件）
 """
 
 
 def pytest_collection_modifyitems(items):
     """
     测试用例收集完成时，将收集到的item的name和nodeid的中文显示
-    :return:
+    :return: None
     """
     for item in items:
         item.name = item.name.encode("utf-8").decode("unicode_escape")
         item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape")
-
-
-"""
-防止测试用例标题ids中文两乱码
-"""
-
